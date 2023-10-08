918a6e61599ee4844e7d5fe2b262d7dc5f08ba4f
Unilca <113147796+Unilca@users.noreply.github.com>
2023-10-09 00:22:59 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./styles.scss";
+import "./styles.scss";
 
 const onClickEvent = (e) => {
   e.preventDefault();
@@ -8,10 +8,33 @@ const onClickEvent = (e) => {
 
 const App = () => {
   return (
-    <div className={styles.content}>
-      <div className={styles.label}>Create React App Without CRA😊</div>
+    <div className="content">
+      <div className={styles.label}>Tea & Juice & IceCream 😊</div>
+      <div className={styles.slider}>
+        <div className={styles.slide}>
+          <img
+            className={styles.img}
+            src="https://vkusnoitochka.ru/resize/290x286/upload/iblock/450/si3zz7y9y94unt6djx2gw221dc9nx0jh/large.png"
+            alt="tea"
+          />
+          <img
+            className={styles.img}
+            src="https://vkusnoitochka.ru/resize/290x286/upload/iblock/f67/3kqlujh1qx1oqw5hixxf7jc5lur97dva/large.png"
+            alt="juice"
+          />
+          <img
+            className={styles.img}
+            src="https://vkusnoitochka.ru/resize/290x286/upload/iblock/38c/0ii7grmoxo058li8pjwdap6kh36hn1qe/large.png"
+            alt="icecream"
+          />
+        </div>
+      </div>
+      <div className={styles.label}>Price$</div>
       <button className={styles.btn} onClick={onClickEvent}>
-        Click Me 😎
+        Next
+      </button>
+      <button className={styles.btn} onClick={onClickEvent}>
+        Buy 😎
       </button>
     </div>
   );
